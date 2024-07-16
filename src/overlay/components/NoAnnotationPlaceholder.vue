a2f83abedbb69a0f68e6efec06d58cf85e1e1aad
SaltyEgg <hzhou.me@saltyee.com>
2024-07-15 20:52:03 -0700

    make no annotation div fixed width

--- src/overlay/components/NoAnnotationPlaceholder.vue
+++ src/overlay/components/NoAnnotationPlaceholder.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="card">
+  <div class="card no-annotation">
     <img class="card-img-top" :src="image" alt="Card cap" />
     <div class="card-body text-center">
-      <h5 class="card-title">You do not have any notes for this page! ;-)</h5>
+      <h5 class="card-title">You do not have any annotation for this page! ;-)</h5>
     </div>
   </div>
 </template>
@@ -12,8 +12,14 @@ export default {
   name: 'NoAnnotationPlaceholder',
   data() {
     return {
-      image: chrome.runtime.getURL('icons/dog.jpg'),
+      image: chrome.runtime.getURL('icons/notfound.png'),
     };
   },
 };
 </script>
+
+<style scoped lang="scss">
+.card.no-annotation {
+  width: 358px !important;
+}
+</style>
